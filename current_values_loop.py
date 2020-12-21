0aaf81a16af70ab1cf0dce0f3a9e7daeb6ba673f
Andre Melo <andre.melo@segfy.com>
2020-12-21 10:38:20 -0300

    showing sum

--- current_values_loop.py
+++ current_values_loop.py
@@ -9,21 +9,24 @@ device = ADXL355(1, 2)
 # first_y = device.get_acc_y()
 # first_z = device.get_acc_z()
 
-first_x = 3.66521484375
-first_y = 4.089890625
-first_z = 3.15569921875
+#first_x = 3.66521484375
+#first_y = 4.089890625
+#first_z = 3.15569921875
 
 while True:
     print("ID: {:X}".format(device.get_devid()))
     print(f'TEMP: \t {device.get_temp()}')
-    
-    x = device.get_acc_x() 
-    y = device.get_acc_y() 
-    z = device.get_acc_z() 
-    
-    print(f'X: {round(abs(x - first_x), 2)}')
-    print(f'Y: {round(abs(y - first_y), 2)}')
-    print(f'Z: {round(abs(z - first_z), 2)}')
+
+    x = device.get_acc_x()
+    y = device.get_acc_y()
+    z = device.get_acc_z()
+
+    sum_acc = x + y + z
+
+    print(f'X: {round(x, 2)}')
+    print(f'Y: {round(y, 2)}')
+    print(f'Z: {round(z, 2)}')
+    print(f'sum: {round(sum_acc, 2)}')
 
     print()
 
